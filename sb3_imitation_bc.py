b5bf1e8b64d4811b7e24e19494bbfd30709bc65d
Tudor <tudorjnu@pop-os.localdomain>
2023-01-31 10:45:55 +0000

    cleaned bc

--- sb3_imitation_bc.py
+++ sb3_imitation_bc.py
@@ -1,87 +1,53 @@
-"""This is a simple example demonstrating how to clone the behavior of an expert.
-
-Refer to the jupyter notebooks for more detailed examples of how to use the algorithms.
-"""
-
-import gym
 import numpy as np
 from pathlib import Path
 
-from stable_baselines3 import PPO
+from utils import process_transitions, make_env
+
 from stable_baselines3.common.evaluation import evaluate_policy
-from stable_baselines3.common.vec_env import DummyVecEnv
-from stable_baselines3.ppo import MlpPolicy
-
 from imitation.algorithms import bc
-from imitation.data import rollout
-from imitation.data.wrappers import RolloutInfoWrapper
-from imitation.data import types
-from wrapper_gym import _flatten_obs
-
-from wrapper_gym import DMEnv
-from utils import process_transitions
-from gym.wrappers import TimeLimit, RecordVideo
-from dm_control import composer
-from cathsim import Navigate, Tip, Guidewire, Phantom
-from sb3_algos import ALGOS
-
-env = gym.make("CartPole-v1")
-rng = np.random.default_rng(0)
 
 
-def env_creator():
-    render_kwargs = {'width': 64, 'height': 64}
-    phantom = Phantom("assets/phantom4.xml", model_dir="./assets")
-    tip = Tip(n_bodies=4)
-    guidewire = Guidewire(n_bodies=80)
-    task = Navigate(
-        phantom=phantom,
-        guidewire=guidewire,
-        tip=tip,
+if __name__ == "__main__":
+
+    rng = np.random.default_rng(0)
+
+    env = make_env(
+        flatten_obs=True,
+        time_limit=200,
+        normalize_obs=False,
+        frame_stack=1,
+        render_kwargs=None,
+        env_kwargs=None,
+        gym_version='gym',
+        wrap_monitor=True,
     )
-    env = composer.Environment(
-        task=task,
-        random_state=np.random.RandomState(42),
-        strip_singleton_obs_buffer_dim=True,
+
+    trial_path = Path.cwd() / "rl" / "expert" / "trial_1"
+    transitions = process_transitions(trial_path)
+
+    bc_trainer = bc.BC(
+        observation_space=env.observation_space,
+        action_space=env.action_space,
+        demonstrations=transitions,
+        rng=rng,
     )
-    env = DMEnv(
-        env=env,
-        from_pixels=False,
-        render_kwargs=render_kwargs,
-        channels_first=True,
+
+    reward, _ = evaluate_policy(
+        bc_trainer.policy,  # type: ignore[arg-type]
+        env,
+        n_eval_episodes=3,
     )
-    env = TimeLimit(env, max_episode_steps=200)
-    return env
+    print(f"Reward before training: {reward}")
 
+    print("Training a policy using Behavior Cloning")
+    bc_trainer.train(n_epochs=200)
 
-env = env_creator()
+    bc_trainer.save("./rl/checkpoint/bc_model")
 
-trial_path = Path.cwd() / "rl" / "expert" / "trial_0"
-transitions = process_transitions(trial_path)
+    reward, _ = evaluate_policy(
+        bc_trainer.policy,  # type: ignore[arg-type]
+        env,
+        n_eval_episodes=3,
+    )
 
-bc_trainer = bc.BC(
-    observation_space=env.observation_space,
-    action_space=env.action_space,
-    demonstrations=transitions,
-    rng=rng,
-)
-
-reward, _ = evaluate_policy(
-    bc_trainer.policy,  # type: ignore[arg-type]
-    env,
-    n_eval_episodes=3,
-)
-print(f"Reward before training: {reward}")
-
-print("Training a policy using Behavior Cloning")
-bc_trainer.train(n_epochs=1)
-
-bc_trainer.save("./rl/checkpoint/bc_model")
-
-reward, _ = evaluate_policy(
-    bc_trainer.policy,  # type: ignore[arg-type]
-    env,
-    n_eval_episodes=3,
-)
-
-print(f"Reward after training: {reward}")
+    print(f"Reward after training: {reward}")
